7b2667d7087fb77dadf8a1e7391420db01d4e3ea
kejee1917 <kejee1917@gmail.com>
2024-08-07 20:28:53 +0900

    [section3] 객체지향 프로그래밍

---
+++ src/oop1/ValueData.java
@@ -0,0 +1,13 @@
+package oop1;
+
+public class ValueData {
+
+    // 멤버변수 필드
+    int value;
+
+    // 메서드
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value=" + value);
+    }
+}
